7ebced77086371c270e7532c8ddad7b533b27d9f
behzuzzbek <behruztillayev0724@gmail.com>
2024-02-08 11:51:00 -1200

    exem-3

---
+++ script.js
@@ -0,0 +1,53 @@
+    // function toggleDarkLight() {
+    //     var body = document.getElementById("body");
+    //     var currentClass = body.className;
+    //     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";   
+    //   }
+
+
+
+
+      function myFunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+     }
+// function darkmode() {
+//   const body = document.body
+//   const wasDarkmode = localStorage.getItem('darkmode') ==='true'
+
+//   localStorage.setItem('darkmode', wasDarkmode)
+// body.classList.toggle('dark-mode', wasDarkmode)
+// }
+// document.querySelector('.btn').addEventListener('click', darkmode)
+
+
+
+
+
+
+
+
+
+
+
+//       var swiper = new Swiper(".mySwiper", {
+//         rewind: true,
+//         autoplay: {
+//           delay: 2000,
+//           disableOnInteraction: true,
+//          },
+        
+//         pagination: {
+//               el: '.swiper-pagination',
+//           },
+        
+//         navigation: {
+//           nextEl: ".swiper-button-next",
+//           prevEl: ".swiper-button-prev",
+//         },
+
+//         scrollbar: {
+//               el: '.swiper-scrollbar',
+//         },
+//       });
+  
